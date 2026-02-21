144167b13a34d4d59f02bdf26417c693b1a30b96
LucaLeonx <lucaleonzio12@gmail.com>
2026-02-21 22:47:57 +0100

    Added thread affinity and modified testSPSC

---
+++ include/thread_utils.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <sys/syscall.h>
+
+/// Set the current thread to a fixed CPU core defined by core_id
+inline bool setThreadAffinity(int core_id) noexcept{
+    cpu_set_t cpu_set;
+    CPU_ZERO(&cpu_set);
+    CPU_SET(core_id, &cpu_set);
+
+    return pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpu_set) == 0;
+}   
+
+///Create Thread, set its affinity and passes the function through perfer forwarding
+template<typename T, typename... A>
+inline auto createAndStartThread(int core_id, const std::string& name, T&& func, A&& ...args) noexcept{
+
+    auto threadBody = [&](){
+        if(core_id >= 0 && !setThreadAffinity(core_id)){
+            std::cerr<<"Failed to set thread affinity for thread: "<< name << " to core: "<< core_id <<std::endl;
+            return;
+        }
+        std::cout<<"Thread affinity set for thread: "<< name << " to core: "<< core_id <<std::endl;
+
+        std::forward<T>(func) (std::forward<A>(args)...);
+    };
+
+    std::thread t(threadBody);
+
+    using namespace std::literals::chrono_literals;
+    std::this_thread::sleep_for(1s);
+
+    return t;
+}
--- test/testSPSCQueue.cpp
+++ test/testSPSCQueue.cpp
@@ -2,14 +2,15 @@
 #include <thread>
 
 #include "spsc_queue.hpp"
+#include "thread_utils.hpp"
 
 using namespace std::literals::chrono_literals;
 
 int main(){
-    spscQueue<int> lfQueue(100);
+    spscQueue<int> lfQueue(30);
 
-    std::thread consumer([&lfQueue] (){
-        std::this_thread::sleep_for(1s); 
+    auto consumer = [&lfQueue] (){
+        std::this_thread::sleep_for(5s); 
         while(lfQueue.size()){
             auto el = lfQueue.getNextRead();
             if(el != nullptr ) lfQueue.updateNextRead();
@@ -18,18 +19,21 @@ int main(){
                 break;
             }
             std::cout<<"Thread "<< std::this_thread::get_id() <<" consumed data: "<< *el <<"\t queue still has: " << lfQueue.size() << " elements."<<std::endl;
+            std::this_thread::sleep_for(2s); 
+        
         }
-    });
+    };
 
-    std::thread producer([&lfQueue] (){ 
-        std::this_thread::sleep_for(1s);
-        for(int i{}; i < 90; ++i){    
-            *lfQueue.getNextWrite() = 10;
-            lfQueue.updateNextWrite();
-            std::cout<<"Thread "<< std::this_thread::get_id() <<" produced data: "<< 10 <<"\t queue now has: " << lfQueue.size() << " elements."<<std::endl;
-        }
-    });
+    auto c = createAndStartThread(7, "consumerThread", consumer);
+    
+    for(int i{}; i < 20; ++i){    
+        *lfQueue.getNextWrite() = 10;
+        lfQueue.updateNextWrite();
+        std::cout<<"Thread "<< std::this_thread::get_id() <<" produced data: "<< 10 <<"\t queue now has: " << lfQueue.size() << " elements."<<std::endl;
+        std::this_thread::sleep_for(2s);
+    }
 
-    consumer.join();
-    producer.join();
+    c.join();
+     
+    return 0;
 }
